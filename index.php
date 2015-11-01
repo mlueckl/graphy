d6ee4fbeb72eadac0e5b30c212af2cff68b7ed62
mike <lueckl.mike@gmail.com>
2015-11-01 01:31:29 +0100

    Handle Response

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
 		</div>
 		<script type="text/javascript">
 		window.onload = Init;
+		var count = 0;
+		var totalLength = 0;
 
 		function Init(){
 			LoadData();
@@ -26,8 +28,8 @@
 			jQuery.get("data.json", function(data) {
 			   console.log("=> Load file and parse to JSON");
 			   for(var i=0; i < data.length; i++){
-			   		$("#count").text(i + "/" + data.length);
-			   		Request(data[i]["db"], data[i]["url"]);	
+				    totalLength = data.length;
+			   		Request(data[i]["db"], data[i]["url"]);
 			   }
 			});
 		}
@@ -42,25 +44,53 @@
 			        param: "q%5B0%5D=9bd597d291349d24cfaaa1372ce061c9&q%5B1%5D=a78d43db809c1b816b1ece087ef176cd&q%5B2%5D=72ade56f8df948a55dce90878d65ed68&q%5B3%5D=9bacef70ee59f7b6ec0fe8f379b3ecf3&q%5B4%5D=a4aad8bdbde7573305b26d0e048339af&q%5B5%5D=73f3349731c05ea293ea0827a7d5dc4b&q%5B6%5D=1bdd0aacde59bbb69da2bc7ac1b08ae3&q%5B7%5D=6fa120615afa4b0cbb8de1be8d20756b&q%5B8%5D=497972b55aa390b4f4fff937a2e6d412&q%5B9%5D=772e5ef21cd037e1b3b3e77bf5cfb3dc",
 			        db: [db]
 			    },
-			    success: Append
+				success: function(data){
+					Append(data);
+					count++;
+					$("#count").text(count + "/" + totalLength);
+				}
 			});
 		}
 
 		function Append(data){
-			var json = jQuery.parseJSON(data[1]);
-			console.log(data);
 
-			if(json == ""){
-				status = "red";
-		    }else if(data[2] < 1){
-		    	status = "green";
-		    }else if(data[2] < 5){
-		    	status = "orange";
-		    }else{
-		    	status = "red";
-		    }
+			if(data[1] != undefined){
+				if(typeof data[1] == "string"){
+					if(data[1].toLowerCase().indexOf("error") > -1 || data[1].toLowerCase().indexOf("forbidden") > -1){
+						var status = "red";
 
-			$(".content").append("<div class='row'><header class='" + status + "'><p>" + data[0] + "</p><p class='right'><b>" + data[2] + "s</b></p></header><div class='example'><p><b>MD5: </b>" + json[0]["md5"] + "</p><p><b>Status: </b>" + json[0]["status"] + "</div></div>");
+						$(".content").append("<div class='row'><header class='" + status + "'><p>" + data[0] + "</p><p class='right'><b>" + data[2] + "s</b></p></header><div class='example'><p>Response:</p><p>" + data[1] + "</p></div></div>");
+					}else{
+						var json = jQuery.parseJSON(data[1]);
+						var arrayIndex = Math.floor(Math.random() * 9) + 1;
+
+						if(json[arrayIndex]["md5"]){
+							if(json == ""){
+								status = "red";
+							}else if(data[2] < 1){
+								status = "green";
+							}else if(data[2] < 5){
+								status = "orange";
+							}else{
+								status = "red";
+							}
+
+							$(".content").append("<div class='row'><header class='" + status + "'><p>" + data[0] + "</p><p class='right'><b>" + data[2] + "s</b></p></header><div class='example'><p><b>MD5: </b>" + json[0]["md5"] + "</p><p><b>Status: </b>" + json[0]["status"] + "</div></div>");
+						}else{
+							var status = "red";
+
+							$(".content").append("<div class='row'><header class='" + status + "'><p>" + data[0] + "</p><p class='right'><b>" + data[2] + "s</b></p></header><div class='example'><p>Response:</p><p>" + data[1] + "</p></div></div>");
+						}
+					}
+				}else{
+					var status = "red";
+
+					$(".content").append("<div class='row'><header class='" + status + "'><p>" + data[0] + "</p><p class='right'><b>" + data[2] + "s</b></p></header><div class='example'><p>Response:</p><p>" + data[1] + "</p></div></div>");
+				}
+			}else{
+				//console.log("Undefined");
+				//console.log(data);
+			}
 		}
 		</script>
 	</body>
